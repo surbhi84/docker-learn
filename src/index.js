1892b979e8af61d4429e946e37a02a3bf35550e4
surbhi84 <56334321+surbhi84@users.noreply.github.com>
2023-09-29 19:58:56 +0530

    added dockerFile and docker compose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const app = require("express")();
 
-app.get('/',(req,res)=>
-res.json({message:'Docker is supposed to be easy'})
+app.get("/", (req, res) =>
+  res.json({ message: "Docker is supposed to be easy" })
 );
 
 const port = process.env.PORT || 8080;
+
+app.listen(port, () =>
+  console.log(`App listening on http://localhost:${port}`)
+);
